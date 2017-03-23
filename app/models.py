a956cedfd6acbbf0b84a5953fdbfa454d0233da6
Vincent Chow <chow_vincent@utexas.edu>
2017-03-22 22:06:50 -0500

    fixed typos in relationships

--- app/models.py
+++ app/models.py
@@ -9,9 +9,9 @@ class Agency(db.Model):
 	countryCode = db.Column(db.String(10))
 	wikiUrl = db.Column(db.String(120))
 	# Many to one for Launch (many launches for an agency)
-	launches = db.relationship('launches', backref = 'agency', lazy = 'dynamic')
+	launches = db.relationship('Launch', backref = 'agency', lazy = 'dynamic')
 	# Many to many for Missions (many agencies for many missions)
-	missions = db.relationship('missions', secondary = associationTable, back_populates='agencies')
+	missions = db.relationship('Mission', secondary = associationTable, back_populates='agencies')
 
 class Launch(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -26,14 +26,14 @@ class Launch(db.Model):
 	# One to many for launch (one location for a launch)
 	location = db.Column(db.Integer, db.ForeignKey('location.id'))
 	# One to one for Mission (one mission for a launch)
-	mission = db.relationship('mission', back_populates = 'launch')
+	mission = db.relationship('Mission', back_populates = 'launch')
 
 class Location(db.Model):
 	id = db.Column(db.Integer, primary_key = True)	
 	name = db.Column(db.String(120), unique = True)
 	countryCode = db.Column(db.String(10))
 	# Many to one for Launch (many launches for a location)
-	launches = db.relationship('launches', backref='location', lazy='dynamic')
+	launches = db.relationship('Launch', backref = 'location', lazy = 'dynamic')
 
 class Mission(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -42,9 +42,9 @@ class Mission(db.Model):
 	typeName = db.Column(db.String(120))
 	wikiUrl = db.Column(db.String(120))
 	# Many to many for Agencies (many agencies for many missions)
-	agencies = db.relationship('agencies', secondary = associationTable, back_populates = 'missions')
+	agencies = db.relationship('Agency', secondary = associationTable, back_populates = 'missions')
 	# One to one for Launch (one launch for a mission)
-	launch = db.relationship('launch', uselist = False, back_populates = 'mission')
+	launch = db.relationship('Launch', uselist = False, back_populates = 'mission')
 
 	# agencyName = db.Column(db.String(120))
 	# launchName = db.Column(db.String(120))
