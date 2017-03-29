e7ec00181a11b2e8b45abc916114cf3cf638e0fb
Vincent Chow <chow_vincent@utexas.edu>
2017-03-28 21:23:42 -0500

    Models should work, linked correctly

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ al_associationTable = db.Table('al_association',
 class Agency(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(120), unique = True)
-	abbrev = db.Column(db.String(10), unique = True)
+	abbrev = db.Column(db.String(10))
 	agencyType = db.Column(db.String(120))
 	countryCode = db.Column(db.String(10))
 	wikiUrl = db.Column(db.String(120))
@@ -37,9 +37,9 @@ class Launch(db.Model):
 	location_id = db.Column(db.Integer, db.ForeignKey('location.id')) # Use location_owner = "ex_location" to assign
 	# Many agencies to many launches
 	agencies = db.relationship('Agency', secondary = al_associationTable, 
-		backref = db.backref('launches'), lazy = 'dynamic')
+		backref = db.backref('launches', lazy = 'dynamic'))
 	# One mission for one launch
-	mission = db.relationship('Mission', uselist = False, back_populates = 'launch')
+	mission = db.relationship('Mission', uselist = False, backref = 'launch')
 
 class Location(db.Model):
 	id = db.Column(db.Integer, primary_key = True)	
@@ -58,98 +58,7 @@ class Mission(db.Model):
 
 	# Many agencies to many missions
 	agencies = db.relationship('Agency', secondary = am_associationTable, 
-		backref = db.backref('missions'), lazy = 'dynamic')
+		backref = db.backref('missions', lazy = 'dynamic'))
 
 	# One mission to one launch
-	launch = db.relationship('Launch', back_populates = 'mission')
-	
-	
-
-
-# from app import db
-# #check string size for all of the columns!!
-# # Relational Table for many to many relationship for agency and mission
-# associationTable = db.Table('association',
-# 	db.Column('agency_id', db.Integer, db.ForeignKey('agency.id')),
-# 	db.Column('mission_id', db.Integer, db.ForeignKey('mission.id'))
-# )
-
-# class Agency(db.Model):
-# 	id = db.Column(db.Integer, primary_key = True)
-# 	name = db.Column(db.String(120), unique = True)
-# 	abbrev = db.Column(db.String(10), unique = True)
-# 	agencyType = db.Column(db.String(120))
-# 	countryCode = db.Column(db.String(10))
-# 	wikiUrl = db.Column(db.String(120))
-# 	# Many to one for Launch (many launches for an agency)
-# 	launches = db.relationship('Launch', backref = 'agency', lazy = 'dynamic')
-# 	# Many to many for Missions (many agencies for many missions)
-# 	missions = db.relationship('Mission', secondary = associationTable, back_populates='agencies')
-	
-# 	# def __repr__(self):
-# 	# 	return "<Agency(name='%s', abbrev='%s', countryCode='%s')>" % (self.name, self.abbrev, self.countryCode)
-
-# 	# def getJson(self):
-# 	# 	return json information about an instance
-
-# #>>>>>>> 66fc8a6e1744d34b767d22a4b81c9ee92bce084e
-
-# class Launch(db.Model):
-# 	id = db.Column(db.Integer, primary_key = True)
-# 	name = db.Column(db.String(120), unique = True)
-# 	windowStart = db.Column(db.String(120))
-# 	windowEnd = db.Column(db.String(120))
-# 	videoUrl = db.Column(db.String(120))
-# 	launchPad = db.Column(db.String(120))
-# 	rocket = db.Column(db.String(120))
-# 	# One to many for Agency (one agency for a launch)
-# 	agency = db.Column(db.Integer, db.ForeignKey('agency.id'))
-# 	# One to many for launch (one location for a launch)
-# 	location = db.Column(db.Integer, db.ForeignKey('location.id'))
-# 	# One to one for Mission (one mission for a launch)
-# #	mission = db.relationship('Mission', back_populates = 'launch')
-
-# 	# def __repr__(self):
-# 		# return "<Launch(name='%s')>" % (self.name)
-
-# 	# def getJson(self):
-# 	# 	return json information about an instance	
-
-# class Location(db.Model):
-# 	id = db.Column(db.Integer, primary_key = True)	
-# 	name = db.Column(db.String(120), unique = True)
-# 	countryCode = db.Column(db.String(10))
-# 	# Many to one for Launch (many launches for a location)
-# #	launches = db.relationship('Launch', backref = 'location', lazy = 'dynamic')
-
-# 	# def __repr__(self):
-# 		# return "<Location(name='%s')>" % (self.name)
-
-# 	# def getJson(self):
-# 	# 	return json information about an instance
-
-# 	# def getLaunches(self):
-# 		# return all launches associated with a location		
-
-# class Mission(db.Model):
-# 	id = db.Column(db.Integer, primary_key = True)
-# 	name = db.Column(db.String(120), unique = True)
-# 	description = db.Column(db.String(400))
-# 	typeName = db.Column(db.String(120))
-# 	wikiUrl = db.Column(db.String(120))
-# 	# Many to many for Agencies (many agencies for many missions)
-# #	agencies = db.relationship('Agency', secondary = associationTable, back_populates = 'missions')
-# 	# One to one for Launch (one launch for a mission)
-# #	launch = db.relationship('Launch', uselist = False, back_populates = 'mission')
-
-# 	# def __repr__(self):
-# 		# return "<Mission(name='%s')>" % (self.name)
-
-# 	# def getJson(self):
-# 	# 	return json information about an instance
-
-# 	# def getLaunch(self):
-# 		# return json information about the launch associated with a mission		
-
-# 	# agencyName = db.Column(db.String(120))
-# 	# launchName = db.Column(db.String(120))
+	launch_id = db.Column(db.Integer, db.ForeignKey('launch.id'))
