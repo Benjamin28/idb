0898626ec8d69cda29f379cccaaac4de1a635034
Vincent Chow <chow_vincent@utexas.edu>
2017-04-01 14:32:09 -0500

    Added check to solve duplicate error, added all data from API to db Fixes #27, Fixes #28, Fixes #30

--- get.py
+++ get.py
@@ -4,7 +4,7 @@ import requests             # Used for http GET request
 import json 				# Used for loading JSON data
 
 # TODO: Add nicer output for empty values, such as in Mission model
-# TODO: Gather more data from API, LOTS of missing values in the Mission.launch
+# TODO: Address what to do with duplicates. Launch gets updated as ID increases, agency doesnt, mission has the most updated on the first
 
 #Global dict that stores the agency types(key: type_id, value: type_name)
 agency_types = {}
@@ -21,7 +21,7 @@ def create_agency_types():
 #-----------------------
 def create_agency():
 	print("Creating agency")
-	agency_response = requests.get("https://launchlibrary.net/1.2/agency")
+	agency_response = requests.get("https://launchlibrary.net/1.2/agency?limit=173")
 	agency_json = json.loads(agency_response.text)
 
 	for agency in agency_json["agencies"]:
@@ -29,12 +29,16 @@ def create_agency():
 		inner_agency_json = json.loads(inner_agency_response.text)
 
 		agency_info = inner_agency_json["agencies"][0]
-		current_agency = Agency(name = agency_info["name"], abbrev = agency_info["abbrev"], agencyType = agency_types[agency_info["type"]], 
-								countryCode = agency_info["countryCode"], wikiUrl = agency_info["wikiURL"])
-		db.session.add(current_agency)
-		# Relationships
-			# Launches should have been added into db with Launch model
-			# Missions should have been added into db with Mission model
+		duplicate_check = db.session.query(Agency).filter_by(name = agency_info["name"]).first()
+		if duplicate_check is None:
+			current_agency = Agency(name = agency_info["name"], abbrev = agency_info["abbrev"], agencyType = agency_types[agency_info["type"]], 
+								countryCode = agency_info["countryCode"], wikiUrl = agency_info["wikiURL"])			
+			db.session.add(current_agency)
+
+			# LAUNCH should have been added into db with Launch model
+			# MISSION should have been added into db with Mission model
+		else:
+			print("Found agency duplicate:", duplicate_check.name)
 
 	db.session.commit()	
 
@@ -43,7 +47,7 @@ def create_agency():
 #-----------------------
 def create_launch():
 	print("Creating launch")
-	launch_response = requests.get("https://launchlibrary.net/1.2/launch")
+	launch_response = requests.get("https://launchlibrary.net/1.2/launch?limit=1088")
 	launch_json = json.loads(launch_response.text)
 
 	for launch in launch_json["launches"]:
@@ -55,30 +59,26 @@ def create_launch():
 		current_videoUrl = launch_info["vidURLs"][0] if len(launch_info["vidURLs"]) > 0 else "None"
 		current_launchPad = launch_info["location"]["pads"][0]["name"] if len(launch_info["location"]["pads"]) > 0 else "None"
 
-		#Relationships
-
-		# LOCATION
-		# Get the one location associated with this launch
+		# LOCATION (Get the one location associated with this launch)
 		current_location = launch_info["location"]["name"]
 		if current_location != "":
 			related_location = db.session.query(Location).filter_by(name = current_location).first()
 
-		current_launch = Launch(name = launch_info["name"], windowStart = launch_info["windowstart"], windowEnd = launch_info["windowend"], 
+		duplicate_check = db.session.query(Launch).filter_by(name = launch_info["name"]).first()
+		if duplicate_check is None:
+			current_launch = Launch(name = launch_info["name"], windowStart = launch_info["windowstart"], windowEnd = launch_info["windowend"], 
 								videoUrl = current_videoUrl, launchPad = current_launchPad, rocket = launch_info["rocket"]["name"],
 								location_owner = related_location)
+			db.session.add(current_launch)
 
-		db.session.add(current_launch)
-
-		# MISSION
-		# Note: Mission should be associated already in Mission model
-
-		# AGENCY
-		# Get the many agencies associated with this launch
-		# TODO: check the agencies, a lot of them don't appear
-		for agency in launch_info["location"]["pads"][0]["agencies"]:
-			related_agency = db.session.query(Agency).filter_by(name = agency["name"]).first()
-			if related_agency is not None:
-				current_launch.agencies.append(related_agency)
+			# MISSION (should be associated already in Mission model)
+			# AGENCY (Get the many agencies associated with this launch)
+			for agency in launch_info["location"]["pads"][0]["agencies"]:
+				related_agency = db.session.query(Agency).filter_by(name = agency["name"]).first()
+				if related_agency is not None:
+					current_launch.agencies.append(related_agency)
+		else:
+			print("Found launch duplicate:", duplicate_check.name)
 
 	db.session.commit()
 
@@ -87,18 +87,19 @@ def create_launch():
 #-----------------------
 def create_location():
 	print("Creating location")
-	location_response = requests.get("https://launchlibrary.net/1.2/location")
+	location_response = requests.get("https://launchlibrary.net/1.2/location?limit=37")
 	location_json = json.loads(location_response.text)
 
 	for location in location_json["locations"]:
 
-		current_location = Location(name = location["name"], countryCode = location["countrycode"])
-		db.session.add(current_location)
+		duplicate_check = db.session.query(Location).filter_by(name = location["name"]).first()
+		if duplicate_check is None:
+			current_location = Location(name = location["name"], countryCode = location["countrycode"])
+			db.session.add(current_location)
 
-		# Relationships
-
-		# LAUNCH
-		# Note: Location.launches should be filled in by Launch model
+			# LAUNCH (Location.launches should be filled in by Launch model)
+		else:
+			print("Found location duplicate:", duplicate_check.name)
 
 	db.session.commit()
 
@@ -107,7 +108,7 @@ def create_location():
 # -----------------------
 def create_mission():
 	print("Creating mission")
-	mission_response = requests.get("https://launchlibrary.net/1.2/mission")
+	mission_response = requests.get("https://launchlibrary.net/1.2/mission?limit=495")
 	mission_json = json.loads(mission_response.text)
 
 	for mission in mission_json["missions"]:
@@ -116,32 +117,30 @@ def create_mission():
 
 		mission_info = inner_mission_json["missions"][0]
 
-		# Relationships
-		# LAUNCH
-		# Get the one launch associated with this mission
-
+		# LAUNCH (Get the one launch associated with this mission)
 		current_launch = mission_info["launch"]["name"] if len(mission_info["launch"]) > 0 else ""
 		if current_launch != "":
 			related_launch = db.session.query(Launch).filter_by(name = current_launch).first()
 
-		current_mission = Mission(name = mission_info["name"], description = mission_info["description"],
+		duplicate_check = db.session.query(Mission).filter_by(name = mission_info["name"]).first()
+		if duplicate_check is None:
+			current_mission = Mission(name = mission_info["name"], description = mission_info["description"],
 								typeName = mission_info["typeName"], wikiUrl = mission_info["wikiURL"],
 								launch = related_launch)
+			db.session.add(current_mission)
 
-		# AGENCY
-		# Get the many agencies associated with this mission
-		# TODO: check the agencies, a lot of them don't appear
-		for agency in mission_info["agencies"]:
-			related_agency = db.session.query(Agency).filter_by(name = agency["name"]).first()
-			if related_agency is not None:
-				current_mission.agencies.append(related_agency)
-
-		db.session.add(current_mission)
+			# AGENCY (Get the many agencies associated with this mission)
+			# TODO: check the agencies, a lot of them don't appear
+			for agency in mission_info["agencies"]:
+				related_agency = db.session.query(Agency).filter_by(name = agency["name"]).first()
+				if related_agency is not None:
+					current_mission.agencies.append(related_agency)
+		else:
+			print("Found mission duplicate:", duplicate_check.name)
 
 	db.session.commit()
 
 def main():
-	print("Inside main")
 	create_agency_types()
 	create_agency()
 	create_location()
