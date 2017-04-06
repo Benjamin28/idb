8951a44a5e1420ab6d15c075c23f54071170f947
Vincent Chow <chow_vincent@utexas.edu>
2017-04-06 10:33:33 -0500

    cleaned up views, added dynamic routing for agencies, ..., etc

--- app/views.py
+++ app/views.py
@@ -10,22 +10,6 @@ import urllib
 def index():
 	return render_template("index.html")
 
-@app.route('/agencies')
-def agencies():
-	return render_template("agencies.html")
-
-@app.route('/launches')
-def launches():
-	return render_template("launches.html")
-
-@app.route('/locations')
-def locations():
-	return render_template("locations.html")
-
-@app.route('/missions')
-def missions():
-	return render_template("missions.html")
-
 @app.route('/about')
 def about():
 	return render_template("about.html")
@@ -137,12 +121,12 @@ def api_model(model):
 	return jsonify(l)
 
 @app.route('/<model>')
-def models(model, page=1):
+def models(model):
 	info = getModel(model)
 	m = info[0]
 	if m == -1:
 		return "<h1>Model not found</h1>"
-	return render_template(info[1]+".html")
+	return render_template(info[1] + ".html")
 
 # @app.route('/<model>')
 # @app.route('/<model>/<int:page>')
@@ -176,68 +160,17 @@ def models(model, page=1):
 #     return request.query_string
 ####
 
-# temperal route to dummy pages 
-# Should be removed later!!!!!
-@app.route('/agencies?id=1')
-def agency_1():
-	return render_template("temp/agency_1.html");
-
-@app.route('/agencies?id=2')
-def agency_2():
-	return render_template("temp/agency_2.html");
-
-@app.route('/agencies?id=3')
-def agency_3():
-	return render_template("temp/agency_3.html");
-
-@app.route('/launches?id=1')
-def launches_1():
-	return render_template("temp/launches_1.html");
-
-@app.route('/launches?id=2')
-def launches_2():
-	return render_template("temp/launches_2.html");
-
-@app.route('/launches?id=3')
-def launches_3():
-	return render_template("temp/launches_3.html");
-
-@app.route('/locations?id=1')
-def location_1():
-	return render_template("temp/location_1.html");
-
-@app.route('/locations?id=2')
-def location_2():
-	return render_template("temp/location_2.html");
-
-@app.route('/locations?id=3')
-def location_3():
-	return render_template("temp/location_3.html");
-
-@app.route('/missions?id=1')
-def mission_1():
-	return render_template("temp/mission_1.html");
-
-@app.route('/missions?id=2')
-def mission_2():
-	return render_template("temp/mission_2.html");
-
-@app.route('/missions?id=3')
-def mission_3():
-	return render_template("temp/mission_3.html");
-
-
 #UTILITY METHODS
 def getModel(model):
-	if model == 'agency':
+	if model == 'agencies':
 		return (Agency, "agencies")
-	elif model == 'launch':
+	elif model == 'launches':
 		return (Launch, "launches")
-	elif model == 'location':
+	elif model == 'locations':
 		return (Location, "locations")
-	elif model == 'mission':
+	elif model == 'missions':
 		return (Mission, "missions")
 	else:
-		return -1
+		return (-1, "Model not found")
 
 
