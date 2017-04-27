d3cb252a10f653e3a6e09fbaddc80ec5fbe98518
Vincent Chow <chow_vincent@utexas.edu>
2017-04-27 09:40:01 -0500

    added CORS for other group to visualize

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
 db = SQLAlchemy(app)
+CORS(app)
 
 from app import views
 
